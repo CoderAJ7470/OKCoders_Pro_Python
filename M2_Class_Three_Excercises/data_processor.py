3fc461c03b836a020723bc762ed6dff7efff928a
AJoshi <ameyasureshjoshi@gmail.com>
2025-12-19 20:52:43 -0600

    Started work on integration project from M2, class three; wip

---
+++ M2_Class_Three_Excercises/data_processor.py
@@ -0,0 +1,47 @@
+# Build a data processing system that does the following:
+
+# Scrapes data from a website
+# Processes the data in a database
+# Generates a report with charts
+# Emails report to recipients with EZGmail
+# Sends a push notification when done with ntfy
+
+import requests, bs4, sqlite3
+
+def get_latest_five_posts():
+    base_item_url = 'https://hacker-news.firebaseio.com/v0/item/{}.json'
+    
+    ids = requests.get(
+        'https://hacker-news.firebaseio.com/v0/newstories.json'
+    ).json()[:5]
+
+    posts = []
+
+    for story_id in ids:
+        story = requests.get(base_item_url.format(story_id)).json()
+        posts.append({
+            'title': story.get('title'),
+            'url': story.get('url'),
+            'by': story.get('by'),
+            'time': story.get('time')
+        })
+
+    print('Latest five posts: ', posts)
+    
+def read_posts_into_db():
+    posts_db = sqlite3.connect('hn_posts.db', isolation_level = None)
+    cursor = posts_db.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS latest_posts (
+            id INTEGER PRIMARY KEY,
+            title TEXT NOT NULL,
+            link TEXT,
+            posted_by TEXT,
+            posted_time INTEGER
+        )
+    ''')
+
+    posts_db.close()
+
+get_latest_five_posts()
